4ee05cef3ca63df304b86c2a2ca52287c26bb22f
winlin <winlin@vip.126.com>
2021-01-05 16:25:29 +0800

    Fix #25, Support SimpleDHTErrCode and SimpleDHTErrDuration

--- examples/DHT11WithRawBits/DHT11WithRawBits.ino
+++ examples/DHT11WithRawBits/DHT11WithRawBits.ino
@@ -22,7 +22,8 @@ void loop() {
   byte data[40] = {0};
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(&temperature, &humidity, data)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err); delay(1000);
     return;
   }
   
